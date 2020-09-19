54ba4b8cd2cb38e91ce1d62e5b14330d310042d5
Johnson <jhonsonj88@gmail.com>
2020-09-19 11:47:54 -0700

    created balance component and functon and imported it to App.js

---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+//Generate a function component type rafc
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <>
+            <h4>Your Balance</h4>
+            <h1 id="balance">$0.00</h1>
+        </>
+    )
+}
+
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2v>
+            Expense Tracker 
+        </h2v>
+    )
+}
