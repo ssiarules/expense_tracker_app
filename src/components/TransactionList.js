e2301e497cab29d04e1b24f3d7600cac2384284d
Johnson <jhonsonj88@gmail.com>
2020-09-19 14:59:54 -0700

    updated TransactionList.js file by imported GlobalContext and map each transaction to disply to user

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext} from '../context/GlobalState';
 
 export const TransactionList = () => {
+
+    //Pull anything from our GlobalContext using the useContext Hook
+    const { transactions } = useContext(GlobalContext);
+
+       // console.log(context)
     return (
         <>
              <h3>History</h3>
            <ul className="list">
-             <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li> 
+            {transactions.map(transaction => (<li className="minus">
+          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+        </li> ))}
+             
       </ul>
         </>
     )
